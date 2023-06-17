c984da5c25f08c3a474137cc3573563cedaa7c3a
Faly RAKOTOMAHARO <rakfaly@gmail.com>
2023-06-17 10:56:13 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Faly RAKOTOMAHARO on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
